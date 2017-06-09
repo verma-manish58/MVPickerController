06f54f4ce53d92205fa36d01d42fa4ab18e4e6dd
Manish <m.verma1985@gmail.com>
2017-06-09 16:59:14 +0530

    Code Refactoring

--- MVPickerController/Picker/MVPickerController.m
+++ MVPickerController/Picker/MVPickerController.m
@@ -29,8 +29,7 @@ static MVPickerController *sharedInstance;
 @synthesize target;
 @synthesize tag;
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(hidePicker)
@@ -41,8 +40,7 @@ static MVPickerController *sharedInstance;
 }
 
 #pragma mark- Shared Picker View
-+ (MVPickerController *)sharedInstance
-{
++ (MVPickerController *)sharedInstance {
     static dispatch_once_t once;
     dispatch_once(&once, ^{
         sharedInstance = [[MVPickerController alloc] initWithNibName:@"MVPickerController" bundle:nil];
@@ -51,8 +49,7 @@ static MVPickerController *sharedInstance;
 }
 
 #pragma mark- Show Picker
-- (void)showSharedPickerViewWithItems:(NSArray*)pickerItems delegate:(id)delegate tag:(NSUInteger)pickerTag onView:(id)view
-{
+- (void)showSharedPickerViewWithItems:(NSArray*)pickerItems delegate:(id)delegate tag:(NSUInteger)pickerTag onView:(id)view {
     //not being used here as we are showing picker on top of tab view rather than showing it on a view controller
     UIViewController *superViewController = (UIViewController*)view;
     
@@ -62,41 +59,28 @@ static MVPickerController *sharedInstance;
     self.target         =   delegate;
     
     self.target   =   delegate;
-    
-    if ([self.strDeviceOrientation compare:@"portrait" options:NSCaseInsensitiveSearch] == NSOrderedSame)
-    {
+
+    if ([self.strDeviceOrientation compare:@"portrait" options:NSCaseInsensitiveSearch] == NSOrderedSame){
         [self.view setFrame:CGRectMake(0, HIDDEN_Y_OFFSET, SCREEN_WIDTH, PICKER_HEIGHT)];
         [self.toolBar setFrame:CGRectMake(0, 0, self.view.frame.size.width, 44)];
         
-    }
-    else
-    {
+    }else{
         [self.view setFrame:CGRectMake(0, SCREEN_HEIGHT - PICKER_HEIGHT, SCREEN_WIDTH, PICKER_HEIGHT)];
         [self.toolBar setFrame:CGRectMake(0, 0, self.view.frame.size.width, 44)];
         
         NSLog(@"View Frame: %@", NSStringFromCGRect(self.view.frame));
     }
-    
-    
     [UIView animateWithDuration:ANIMATION_DURATION animations:^{
-        if ([self.strDeviceOrientation compare:@"portrait" options:NSCaseInsensitiveSearch] == NSOrderedSame)
-        {
+        if ([self.strDeviceOrientation compare:@"portrait" options:NSCaseInsensitiveSearch] == NSOrderedSame){
             //            [self.view setFrame:CGRectMake(0, SCREEN_HEIGHT - PICKER_HEIGHT, SCREEN_WIDTH, PICKER_HEIGHT)];
             [self.view setFrame:CGRectMake(0, (IPAD ?superViewController.view.frame.size.height : SCREEN_HEIGHT) - PICKER_HEIGHT, IPAD ? superViewController.view.frame.size.width : SCREEN_WIDTH, PICKER_HEIGHT)];
             [self.toolBar setFrame:CGRectMake(0, 0, self.view.frame.size.width, 44)];
-        }
-        else
-        {
+        }else{
             [self.view setFrame:CGRectMake(0, (IPAD ? superViewController.view.frame.size.height : SCREEN_HEIGHT) - PICKER_HEIGHT, IPAD ? superViewController.view.frame.size.width : SCREEN_WIDTH, PICKER_HEIGHT)];
             [self.toolBar setFrame:CGRectMake(0, 0, self.view.frame.size.width, 44)];
         }
         
         self.isPickerVisible = YES;
-        
-        //        NSLog(@"SCREEN_HEIGHT: %f", SCREEN_HEIGHT);
-        //        NSLog(@"SCREEN_WIDTH: %f", SCREEN_WIDTH);
-        
-        
         [superViewController.view addSubview:self.view];
     }];
     [self.picker setTag:pickerTag];
@@ -104,15 +88,13 @@ static MVPickerController *sharedInstance;
 }
 
 #pragma mark- Filling Picker
-- (void)fillPickerWithItems:(NSArray*)items
-{
+- (void)fillPickerWithItems:(NSArray*)items{
     self.arrPickerItems     =   [NSArray arrayWithArray:items];
     [self.picker reloadAllComponents];
 }
 
 #pragma mark- IBAction Method
 - (IBAction)btnDoneTapped:(id)sender {
-    
     if (self.target && [self.target respondsToSelector:@selector(selectedItem:forTag:)])
         [self.target selectedItem:[self.picker.delegate pickerView:self.picker titleForRow:[self.picker selectedRowInComponent:0] forComponent:0] forTag:self.picker.tag];
     
@@ -120,50 +102,30 @@ static MVPickerController *sharedInstance;
 }
 
 - (IBAction)btnCancelTapped:(id)sender {
-    
-    /*
-     if (self.target && [self.target respondsToSelector:@selector(hidePicker)])
-     {
-     [self.target hidePicker];
-     }
-     */
-    
     [self hidePicker];
-    
 }
 
 #pragma mark- Hide Picker
-- (void)hidePicker
-{
+- (void)hidePicker{
     [UIView animateWithDuration:ANIMATION_DURATION animations:^{
-        
         [self.view setFrame:CGRectMake(0, HIDDEN_Y_OFFSET, SCREEN_WIDTH, PICKER_HEIGHT)];
-        
     } completion:^(BOOL finished) {
         [self.view removeFromSuperview];
-        
-        
     }];
-    
-     self.isPickerVisible = NO;
+    self.isPickerVisible = NO;
 }
 
 
 #pragma mark- UIPickerViewDataSource Methods
-- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
-{
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
     return 1;
 }
 
-- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
-{
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
     return [self.arrPickerItems count];
 }
 
-//uncomment the code below to use custom rows in pickerview
-/*
- - (UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(UIView *)view
- {
+ - (UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(UIView *)view{
  UILabel *label = [[UILabel alloc] init];
  [label setFrame:CGRectMake(pickerView.center.x, pickerView.center.y, pickerView.frame.size.width, 30)];
  [label setTextAlignment:NSTextAlignmentCenter];
@@ -171,7 +133,7 @@ static MVPickerController *sharedInstance;
  label.textColor = [UIColor whiteColor];
  return label;
  }
- */
+
 
 #pragma mark- UIPickerViewDelegate Methods
 - (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
