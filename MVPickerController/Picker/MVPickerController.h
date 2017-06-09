ca711804d398f415c4896991cb53f5ab742196e3
Manish Punchh <manish@punchh.com>
2017-06-09 17:09:45 +0530

    Some more code re-factoring

--- MVPickerController/Picker/MVPickerController.h
+++ MVPickerController/Picker/MVPickerController.h
@@ -9,37 +9,24 @@
 #import <UIKit/UIKit.h>
 
 @protocol MVPickerControllerDelegate <NSObject>
-
 - (void)selectedItem:(NSString*)item forTag:(NSUInteger)itemForTag;
-
-
 @end
 
 #define APPDELEGATE         ((AppDelegate*)[UIApplication sharedApplication].delegate)
 
 @interface MVPickerController : UIViewController <UIPickerViewDataSource, UIPickerViewDelegate>
-
 @property (assign, nonatomic) BOOL isPickerVisible;
-
 @property (assign, nonatomic) NSUInteger tag;
-
 @property (assign, nonatomic) id <MVPickerControllerDelegate>target;
-
 @property (retain, nonatomic) NSArray *arrPickerItems;
-
 @property (retain, nonatomic) IBOutlet UIPickerView *picker;
-
 @property (weak, nonatomic) IBOutlet UIToolbar *toolBar;
-
 @property (strong, nonatomic) NSString *strDeviceOrientation;
 
 - (void)fillPickerWithItems:(NSArray*)items;
-
 - (IBAction)btnDoneTapped:(id)sender;
 - (IBAction)btnCancelTapped:(id)sender;
-
 - (void)hidePicker;
-
 + (MVPickerController *)sharedInstance;
 - (void)showSharedPickerViewWithItems:(NSArray*)pickerItems delegate:(id)delegate tag:(NSUInteger)pickerTag onView:(id)view;
 
